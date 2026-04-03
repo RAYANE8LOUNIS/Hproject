1ad127172c3748a503d0d02d08e58b0c363961f6
BaggioC04 <mbaggioo40@gmail.com>
2026-04-03 11:07:01 +0100

    Added teams pages, search, templates and routing

---
+++ Hproject-backend/teams/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.team_list, name='team_list'),
+    path('<int:team_id>/', views.team_detail, name='team_detail'),
+]
--- Hproject-backend/teams/views.py
+++ Hproject-backend/teams/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from organisation.models import Team, Dependency
 
-# Create your views here.
+
+def team_list(request):
+    query = request.GET.get('q')
+
+    if query:
+        teams = Team.objects.filter(name__icontains=query)
+    else:
+        teams = Team.objects.all()
+
+    return render(request, 'teams/team_list.html', {
+        'teams': teams,
+        'query': query
+    })
+
+
+def team_detail(request, team_id):
+    team = get_object_or_404(Team, id=team_id)
+
+    members = team.members.all() if hasattr(team, 'members') else []
+    dependencies = Dependency.objects.filter(
+        upstream_team=team
+    ) | Dependency.objects.filter(
+        downstream_team=team
+    )
+
+    return render(request, 'teams/team_detail.html', {
+        'team': team,
+        'members': members,
+        'dependencies': dependencies
+    })
