d92cab65c8fb7993e4737c9a21235653aa921789
RAYANE8LOUNIS <rayanelounis@icloud.com>
2026-04-28 23:12:29 +0100

    Fix urls, settings and organisation templates

--- Hproject-backend/Hproject/settings.py
+++ Hproject-backend/Hproject/settings.py
@@ -41,7 +41,7 @@ ROOT_URLCONF = 'Hproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Hproject-backend/Hproject/urls.py
+++ Hproject-backend/Hproject/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
     path('organisation/', include('organisation.urls')),
     path('teams/', include('teams.urls')),
     path('messaging/', include('messaging.urls')),
